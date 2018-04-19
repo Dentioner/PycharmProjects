3c06374d176b79a5808cb9276bccca869b7e7b08
Dentioner <842007656@qq.com>
2018-04-19 21:54:28 +0900

    Nothing more
    _str_  in class

---
+++ 4.19.py
@@ -0,0 +1,27 @@
+# coding :utf-8
+class ball:
+    def __init__(self, color, size, direction):
+        self.color = color
+        self.size = size
+        self.direction = direction
+
+    def __str__(self):
+        msg = 'hi, I am a '+ self.size+' '+ self.color+ ' ball!'
+        return  msg
+
+    def bounce(self):
+        if self.direction == 'down':
+            self.direction = 'up'
+
+
+myball = ball('red', 'small', 'down')
+
+
+print 'I just created a ball'
+print 'My ball is', myball.size
+print 'My ball is', myball.color
+print 'my ball\'s direction is', myball.direction
+print 'Now I am going to bounce the ball'
+myball.bounce()
+print 'now the ball\'s direction is', myball.direction
+print myball
