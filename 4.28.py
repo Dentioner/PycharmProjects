b3971b25d664a7bf59e7a7358ace6dc4261a8420
Dentioner <842007656@qq.com>
2018-04-28 19:58:33 +0900

    Until p.283
    startwith & endwith

---
+++ 4.28.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# list = ['My','wife','is','Dyson']
+# string = ' '.join(list)
+# print(string)
+
+name = raw_input('what\'s your name?')
+if name.startswith('Dyson') or name.endswith('Hime') == True:
+    print 'Waifu~~~~~~'
+else:
+    print 'guna'
