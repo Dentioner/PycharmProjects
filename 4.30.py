a88a3178fc4285d3f7c2a4b18a958ca2c9591202
Dentioner <842007656@qq.com>
2018-04-30 19:50:41 +0900

    Until chap.22
    str.strip()

---
+++ 4.30.py
@@ -0,0 +1,11 @@
+# coding :utf-8
+n = 'ywwuyi                                '
+print n +'here'
+n = n.strip()
+print n +'here'
+print n.lower()
+print n.upper()
+if n.lower() == n.upper():
+    print 'ok'
+else:
+    print 'rua'
