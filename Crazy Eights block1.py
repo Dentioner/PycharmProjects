001e65069f9bf00dc9b583682bfd705ceab0d6a6
Dentioner <842007656@qq.com>
2018-05-15 21:26:08 +0900

    A little work
    Default

--- Crazy Eights block1.py
+++ Crazy Eights block1.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 game_done = False
-p_hand = 114514
-c_hand = 1919
+p_hand = '114514'
+c_hand = '1919'
 def init_cards():
 
     print 'emm...'
@@ -14,7 +14,7 @@ def computer_turn():
 
 # 以上几行的定义不属于书上写的主程序，只是为了不想让程序报错
 init_cards()
-while not game_done: # while 在 后面的值为True时跳出循环
+while not game_done: # while 在 后面的值为True时开始循环
     blocked = 0 # 这个变量是衡量一方玩家是否能继续出牌，如果有一方出不了牌了，这个值就+1
     player_turn() # 这个是玩家回合的函数，未写
     if len(p_hand) == 0: # p_hand是 玩家手牌数
---
+++ Crazy Eights block2.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+done = False
+p_total = c_total = 0
+while not done:
+    'block1.py'
+    print 'emm'
+    play_again = raw_input('Play Again?-Y/N')
+    if play_again.lower().startswith('y'):  # 用lower和startwith是来确定各种各样的yes的，只要首字母为y即可继续游戏
+         done = False
+    else:
+        done = True
+# 此代码块用来判断是否多次进行游戏
