ff580f9c5eefc362f903623227ccca3dbca19c7e
Dentioner <842007656@qq.com>
2018-05-18 20:45:59 +0900

    Programming
    Default

--- Crazy Eights block3.py
+++ Crazy Eights block3.py
@@ -2,9 +2,13 @@
 from cards import PokerCards
 import random
 deck = []
+blocked = 0
 up_card = ''  # up_card 实际上不是字符串，是一个实例，但是这里就随便定义一下，不让系统报错就行
 active_symbol = ''
 hand = []
+valid_play = False
+flower_database = ['♠', '♥', '♣', '♦']
+
 for i in range(5):
     card = random.choice(deck)
     hand.append(card)
@@ -23,15 +27,42 @@ if up_card.number == 8:
 chosen_number = raw_input('准备出哪张？1,2,3,4,5?不出而要抽牌的话，请输入0')
 chosen_number = int(chosen_number)
 if chosen_number == 0:  # 这表示抽牌的程序
-    new_card = random.choice(deck)
-    hand.append(new_card)
-    deck.remove(new_card)
+    if len(deck) > 0:
+        new_card = random.choice(deck)
+        print '你抽了'+ new_card.ShortName
+        hand.append(new_card)
+        deck.remove(new_card)
+
+    else:
+        print '牌组已空'
+        blocked += 1  # 这一个if块 是为了检测是否还有牌可以抽，否则就判定为一方玩家无法行动了
+
 else:
-    chosen_number -= 1
-    chosen_card = hand[chosen_number]
+    while not valid_play:
+        chosen_number -= 1
+        chosen_card = hand[chosen_number]
+        if chosen_card.number == 8:
+            valid_play = True
+
+            flower_choice = raw_input('请选择你想要的花色，按照顺序为♠，♥，♣，♦，输入1,2,3,4')
+            flower_choice = int(flower_choice) -1
+            active_symbol = flower_database[flower_choice]
+
+            # is_eight = True
+
+
+        elif chosen_card.symbol == active_symbol:
+            #  这里判断标准是否应该再加上一条“出相同点数而不是相同花色”的判断？
+            valid_play = True
+            active_symbol = chosen_card.symbol  # active_card 用来表示出的牌的花色
+
     hand.remove(chosen_card)
     up_card = chosen_card  # up_card 是明牌，展示自己出的牌来作为对方将要参考的牌
     print '你出了' + up_card.ShortName
 
-    active_symbol = chosen_card.symbol  # active_card 用来表示出的牌的花色
-# 这里没有设计 考虑出8的情况，8可以自己选择花色
+# active_symbol = chosen_card.symbol  # active_card 用来表示出的牌的花色
+
+
+# 本代码块的很多地方都没有设定检测非法输入的功能
+# 需要写入一些while循环来保证游戏的正常进行
+# 本代码块也没有设置好，出相同点数的选项判断，5.18为止，只是完成了对“出相同花色”的规则的判断
--- Crazy Eights block4.py
+++ Crazy Eights block4.py
@@ -1,1 +1,40 @@
 # coding: utf-8
+# 这个是抽牌部分的一个示范代码块
+import random
+valid_play = True
+deck = []
+blocked = 0
+p_hand = []
+print '准备干啥?'
+response = raw_input('Type a card to play or \'Draw\'to take a card:')
+while not valid_play :
+    selected_card = None
+    while selected_card == None:
+        if response.lower() == 'draw':
+            valid_play = True
+            if len(deck) > 0:  # 抽牌
+
+                card = random.choice(deck)# 抽牌
+                p_hand.append(card)# 抽牌
+                deck.remove(card)# 抽牌
+                print 'you drew', card.ShortName# 抽牌
+
+            else:# 抽牌
+                print 'no card'# 抽牌
+                blocked += 1# 抽牌
+
+            return
+            # 抽完牌就回到主循环
+        else:
+            for card in p_hand:# 出牌
+                if response.upper() == card.ShortName:# 出牌
+                    selected_card = card# 出牌
+            if selected_card == None:# 出牌
+                response = raw_input('you do not have it. Try again:')# 出牌
+
+    if selected_card.rank == '8':
+        valid_play = True
+        is_eight = True
+    elif selected_card.symbol == active_symbol: # 判断出牌是否合法
+        valid_play = True
+
