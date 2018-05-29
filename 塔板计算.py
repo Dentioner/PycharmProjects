6eac0efe86917b9de98b068a9d3762a89848aa30
Dentioner <842007656@qq.com>
2018-05-29 21:11:13 +0900

    plate calculator
    a small tool for analytical chemistry

---
+++ 塔板计算.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+def ta_ban(K):
+    init_c = 100.0
+    ci_shu = raw_input('分配次数=')
+    ci_shu = float(ci_shu)
+    density = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    m_density = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    i = float(1)
+    K = float(K)
+    ratio = K/(K+1)
+    # print ratio
+    # print '==='
+    while i < ci_shu:
+
+        density[0] = init_c*(ratio**i)
+        m_density[0] = init_c*((1-ratio)**i)
+        # print ratio
+        # print density[0]
+        # print m_density[0]
+
+        density[0] = float('%.2f' % density[0])
+        m_density[0] = float('%.2f' % m_density[0])
+        for j in range(7, 0, -1):
+            density[j] = density[j] + m_density[j-1] - m_density[j]
+            density[j] = float('%.2f' % density[j])
+            m_density[j] = density[j]*(1-ratio)
+            m_density[j] = float('%.2f' % m_density[j])
+            # print 'j=%d'%j
+            # print density[j]
+            # print m_density[j]
+            # print
+        print '第%d次：'%i,
+        print density
+        # print m_density
+        print
+        i +=1
+
+
+k = raw_input('分配系数K=？')
+k = float(k)
+ta_ban(k)
--- 测试.py
+++ 测试.py
@@ -1,17 +1,5 @@
 # coding: utf-8
-from cards import PokerCards
-i = 0
-j = 0
-while j <5:
-    while i < 20:
-        print i
-        i += 1
-        if i == 5:
-            break
-    print
-    print j
-    print 'jjj'
-    print
-    j += 1
+import math
 
-print 'fuck'
+a = 1.0/2.0
+print a
