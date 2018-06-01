aa91e1d1f551174b8dccc810cd48062a96274c22
Dentioner <842007656@qq.com>
2018-06-01 19:39:25 +0900

    Default

--- 塔板计算.py
+++ 塔板计算.py
@@ -1,33 +1,40 @@
 # coding: utf-8
 from __future__ import division
+
+
 def ta_ban(K):
     plate = open('plate.txt', 'w')
     init_c = 100.0
     ci_shu = raw_input('分配次数=')
-    ci_shu = float(ci_shu) + 1
+    ci_shu = float(ci_shu)
     density = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     m_density = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    i = float(1)
+    i = float(0)
     K = float(K)
     ratio = K/(K+1)
 
     while i < ci_shu:
-
         density[0] = init_c*(ratio**i)
-        m_density[0] = init_c*((1-ratio)**i)
-
+        m_density[0] = density[0]*(1-ratio)
         density[0] = float('%.10f' % density[0])
         m_density[0] = float('%.10f' % m_density[0])
-
         for j in range(7, 0, -1):
             density[j] = density[j] + m_density[j-1] - m_density[j]
             density[j] = float('%.10f' % density[j])
             m_density[j] = density[j]*(1-ratio)
             m_density[j] = float('%.10f' % m_density[j])
 
+
+        i += 1
+        density[0] = init_c*(ratio**i)
+        m_density[0] = density[0]*(1-ratio)
+
         print '第%d次：' %i,
-        print density[7]
-        a = density[7]
+
+        print m_density[6]
+
+        a = m_density[6]
+
         a = str(a)
         plate.write(a+'\n')
 
@@ -35,10 +42,11 @@ def ta_ban(K):
         # 如果要输出一个表格的话，就把这一项注释解开，把上面那五行代码变成注释
 
         print
-        i += 1
+
     plate.close()
 
-print 'version 1.2'
+
+print ('version 2.0')
 print
 k = raw_input('分配系数K=？')
 k = float(k)
--- 测试.py
+++ 测试.py
@@ -1,4 +1,9 @@
-from __future__ import division
-b = 1/2
-
-print b
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+aTuple = (123, 'xyz', 'zara', 'abc');
+aList = list(aTuple)
+bTuple = tuple(aList)
+print "列表元素 : ", aList
+print aTuple
+print bTuple
