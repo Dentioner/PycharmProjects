aa2467dbaf9cad62e7c915d979b04e314f55337b
Dentioner <842007656@qq.com>
2018-06-07 18:49:54 +0900

    until p.56
    A little about easygui

---
+++ 6.7.py
@@ -0,0 +1,10 @@
+# coding :utf-8
+# coding:utf-8
+# coding:utf-8
+import easygui
+response = easygui.buttonbox('What\'s your favorite waifu?', choices=['戦艦棲姫', '戦艦水鬼', '戦艦レ級'])
+a = easygui.msgbox('You picked '+ response)
+print response
+print type(response)
+print a
+print type(a)
