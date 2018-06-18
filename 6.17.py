f7328f750b8a680cd2eb609d6ffcad60b0590575
Dentioner <842007656@qq.com>
2018-06-18 20:08:57 +0900

    default

--- 6.17.py
+++ 6.17.py
@@ -9,8 +9,9 @@ for x in range(1, 640):
     y = int(math.sin(x/640.0 * 4 * math.pi)*200 + 240)
     # pygame.draw.rect(screen, [0, 0, 0], [x, y, 1, 1], 1)
     pointlist.append([x, y])
-
+    screen.set_at([x, y], [255, 255, 255])
 pygame.draw.lines(screen, [0, 0, 0], False, pointlist, 2)
+
 pygame.display.flip()
 
 while True:
--- 测试.py
+++ 测试.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
- 
-aTuple = (123, 'xyz', 'zara', 'abc');
-aList = list(aTuple)
-bTuple = tuple(aList)
-print "列表元素 : ", aList
-print aTuple
-print bTuple
+# coding: utf-8
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_caption('hello world!')
+screen = pygame.display.set_mode([640, 480])
+screen.fill([255, 255, 255])
+screen.set_at([150, 150], [255, 0, 0])  # 将150，150改为红色。
+pygame.display.flip()
+print screen.get_at([150, 150])
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
