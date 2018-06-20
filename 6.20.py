bdcf1bb5ebae3b38d52fff523bbc1e78d2a6e2c2
Dentioner <842007656@qq.com>
2018-06-20 20:10:57 +0900

    a small change on the ball pic

---
+++ 6.20.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode([640, 480])
+screen.fill([255, 255, 255])
+
+ball = pygame.image.load('beach_ball.png')
+screen.blit(ball, [0, 0])
+pygame.display.flip()
+pygame.time.delay(2000)
+screen.blit(ball, [100, 50])
+
+pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
