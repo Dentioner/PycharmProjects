5322e558dda44cc9dc9814c2165f4539cb984dce
Dentioner <842007656@qq.com>
2018-06-28 19:58:01 +0900

    a little

--- 测试.py
+++ 测试.py
@@ -1,13 +1,14 @@
 # coding: utf-8
-import pygame, sys
-pygame.init()
-screen = pygame.display.set_caption('hello world!')
-screen = pygame.display.set_mode([640, 480])
-screen.fill([255, 255, 255])
-screen.set_at([150, 150], [255, 0, 0])  # 将150，150改为红色。
-pygame.display.flip()
-print screen.get_at([150, 150])
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+class ceshi:
+    def __init__(self, a, b, c):
+        self.d = a
+        self.e = b
+        self.f = c
+    def __str__(self):
+        return 'fuck you'
+
+z = ceshi(1, 2, 3)
+print z
+
+
+
