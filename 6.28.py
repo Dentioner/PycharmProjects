4cfcdd94625c1271a5039591d5de3beaa231504e
Dentioner <842007656@qq.com>
2018-07-06 19:58:23 +0900

    nothing

--- 6.28.py
+++ 6.28.py
@@ -20,20 +20,44 @@ class MyBall(pygame.sprite.Sprite):
             self.speed[1] = -self.speed[1]
 
 
-# pygame.init()
-size = width, height = 640, 480
-screen = pygame.display.set_mode(size)
-# screen.fill([255, 255, 255])
-pic = 'beach_ball.png'
-balls = []
-# speed = [2, 2]
+def animate(group):
+    screen.fill([255, 255, 255])
+    for a in group:
+        a.move()
+    for a in group:
+        group.remove(a)
+        if pygame.sprite.spritecollide(a, group, False):
+            a.speed[0] = -a.speed[0]
+            a.speed[1] = -a.speed[1]
 
-for hang in range(3):
-    for lie in range(3):
+        group.add(a)
+        # a.move()
+        screen.blit(a.image, a.rect)
+
+
+
+
+
+    pygame.display.flip()
+    pygame.time.delay(20)
+
+
+# pygame.init()
+size = width, height = 1080, 720
+screen = pygame.display.set_mode(size)
+screen.fill([255, 255, 255])
+pic = 'beach_ball.png'
+# balls = []
+# speed = [2, 2]
+group = pygame.sprite.Group()
+
+for hang in range(4):
+    for lie in range(4):
         location = [hang*180 + 10, lie*180 + 10]
-        speed = [choice([-2, 2]), choice([-2, 2])]
+        speed = [choice([-20, 20]), choice([-20, 20])]
         ball = MyBall(pic, location, speed)
-        balls.append(ball)
+        # balls.append(ball)
+        group.add(ball)
 
 # for ball in balls:
 #     ball.move()
@@ -44,10 +68,13 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    pygame.time.delay(20)
-    screen.fill([255, 255, 255])
+    # pygame.time.delay(20)
+    # screen.fill([255, 255, 255])
+    #
+    # for ball in balls:
+    #     ball.move()
+    #     screen.blit(ball.image, ball.rect)
+    # pygame.display.flip()
 
-    for ball in balls:
-        ball.move()
-        screen.blit(ball.image, ball.rect)
-    pygame.display.flip()
+    animate(group)
+
